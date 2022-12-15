1b2b1fad53eb43ac931cadeadbb2e8f1170acd68
Anonav0 <swarnavokhanra@gmail.com>
2022-12-15 13:12:14 -0500

    carmake model created

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,6 +4,41 @@ from django.utils.timezone import now
 
 # Create your models here.
 
+class CarMake(models.Model):
+    name = models.CharField(null= False, max_length=30)
+    description = models.CharField(null= False, max_length=300)
+
+    def __str__(self):
+        return 'Name:' + self.name + ',' + \
+            'Description:' + self.description
+
+
+class CarModel(models.Model):
+    TYPES = (
+            ("SEDAN", "Sedan"), ("SUV", "SUV"), ("WAGON", "Wagon"), ("LIMOUSINE", "Limousine"), ("BATMOBILE", "Batmobile")
+        )
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=30)
+    c_type = models.CharField(max_length=30, choices=TYPES)
+    dealer_id = models.IntegerField()
+    year = models.DateField()
+
+    def __str__(self):
+        return "Name: " + self.name + \
+                " Make Name: "+ self.make.name + \
+                " Type: " + self.c_type + \
+                " Dealer ID: " + str(self.dealer_id)+ \
+                " Year: " + str(self.year)
+
+
+
+
+
+
+
+
+
+
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
 # - Description
