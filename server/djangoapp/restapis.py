cc7f315172a24b2b2e10ec0d0932381135a7e86e
Anonav0 <swarnavokhanra@gmail.com>
2022-12-16 11:41:09 -0500

    get-dealership initial commit

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,7 +1,74 @@
+# import requests
+# import json
+# # import related models here
+# from requests.auth import HTTPBasicAuth
+
 import requests
 import json
-# import related models here
+from .models import CarDealer
 from requests.auth import HTTPBasicAuth
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_watson.natural_language_understanding_v1 import Features,SentimentOptions
+import time
+
+def get_dealers_from_cf(url, **kwargs):
+    results = []
+    state = kwargs.get("state")
+    if state:
+        json_result = get_request(url, state=state)
+    else:
+        json_result = get_request(url)
+
+    if json_result:
+        dealers = json_result["body"]
+        for dealer in dealers:
+            dealer_doc = dealer["doc"]
+            dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"],
+                                id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
+                                full_name=dealer_doc["full_name"], short_name=dealer_doc["short_name"], st=dealer_doc["st"], 
+                                zip=dealer_doc["zip"])
+            results.append(dealer_obj)
+
+    return results
+
+
+def get_request(url, **kwargs):
+    print(kwargs)
+    print("GET from {} ".format(url))
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(url, headers={'Content-Type': 'application/json'},
+                                    params=kwargs)
+    except:
+        # If any error occurs
+        print("Network exception occurred")
+    status_code = response.status_code
+    print("With status {} ".format(status_code))
+    json_data = json.loads(response.text)
+    return json_data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # Create a `get_request` to make HTTP GET requests
